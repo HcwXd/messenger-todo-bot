27e240d82ebdee600381d8ae261cb7bc3d0c22da
HcwXd <e04dl47878@gmail.com>
2019-09-30 17:35:16 +0800

    Add basic todo-bot

---
+++ constant.js
@@ -0,0 +1,5 @@
+module.exports = {
+  ADD_TODO: `action/add_todo`,
+  LIST_TODO: `action/list_todo`,
+  DELETE_TODO: `action/delete_todo`,
+};
---
+++ index.js
@@ -0,0 +1,104 @@
+const { MessengerBot } = require('bottender');
+const { createServer } = require('bottender/express');
+const config = require('./bottender.config').messenger;
+const { ADD_TODO, LIST_TODO, DELETE_TODO } = require('./constant');
+
+const bot = new MessengerBot({
+  accessToken: config.accessToken,
+  appSecret: config.appSecret,
+});
+
+bot.setInitialState({
+  todos: [],
+  isAsking: false,
+  actionType: null,
+});
+
+const sendActionList = async (context) => {
+  await context.sendButtonTemplate('What do you want to do next?', [
+    {
+      type: 'postback',
+      title: 'Add Todo',
+      payload: ADD_TODO,
+    },
+    {
+      type: 'postback',
+      title: 'List Todo',
+      payload: LIST_TODO,
+    },
+    {
+      type: 'postback',
+      title: 'Delete Todo',
+      payload: DELETE_TODO,
+    },
+  ]);
+};
+
+const sendDeleteTodoList = async (context) => {
+  await context.sendButtonTemplate(
+    'Please choose one to delete',
+    context.state.todos.map((todo) => {
+      return {
+        type: 'postback',
+        title: todo,
+        payload: `delete/${todo}`,
+      };
+    })
+  );
+};
+
+bot.onEvent(async (context) => {
+  const user = await context.getUserProfile();
+  console.log(user);
+  if (context.state.isAsking) {
+    if (context.state.actionType === DELETE_TODO && context.event.isPostback) {
+      context.setState({
+        todos: context.state.todos.filter((todo) => todo !== context.event.postback.title),
+        actionType: null,
+        isAsking: false,
+      });
+      await context.sendText(`Delete Todo ${context.event.postback.title}`);
+    } else if (context.state.actionType === ADD_TODO) {
+      context.setState({
+        todos: context.state.todos.concat(context.event.text),
+        actionType: null,
+        isAsking: false,
+      });
+      await context.sendText(`Add Todo ${context.event.text}`);
+    } else {
+      context.setState({
+        actionType: null,
+        isAsking: false,
+      });
+    }
+    await sendActionList(context);
+  } else {
+    if (context.event.isPostback) {
+      switch (context.event.postback.payload) {
+        case ADD_TODO:
+          context.setState({ actionType: ADD_TODO, isAsking: true });
+          await context.sendText(`Please enter your todo`);
+          break;
+        case LIST_TODO:
+          const todoList = context.state.todos.map((todo) => `- ${todo}`).join('\n');
+          await context.sendText(`Your Todo: \n${todoList}`);
+          await sendActionList(context);
+          break;
+        case DELETE_TODO:
+          context.setState({ actionType: DELETE_TODO, isAsking: true });
+          await sendDeleteTodoList(context);
+          break;
+        default:
+          break;
+      }
+    } else {
+      await sendActionList(context);
+    }
+  }
+});
+
+const server = createServer(bot, { verifyToken: config.verifyToken });
+
+server.listen(5000, () => {
+  console.log('server is running on 5000 port...');
+});
