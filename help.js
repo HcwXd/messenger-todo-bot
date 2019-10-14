921cc47011ac406dfbf1ae82585cda3f7ab7e4f2
HcwXd <e04dl47878@gmail.com>
2019-10-14 22:21:26 +0800

    Revise wording of help

--- help.js
+++ help.js
@@ -1,11 +1,16 @@
 const helpText = `
 Welcome to Todo bot!
 
-To add/view a todo, you can simply enter the name of it
+To add or view a todo, you can simply enter the name of it
 
 To view all todos, enter "list"
 
-To edit/delete a todo, enter "list" then choose the todo you want to edit/delete
+To edit or delete a todo, enter "list" then choose the todo you want to edit or delete
+
+By clicking the edit button of a todo, you can add its
+1. Due Date
+2. Reminder
+3. Notes
 
 Also, you can use the buttons on the menu to acheive the actions above
 
