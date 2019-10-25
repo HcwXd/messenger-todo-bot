d38a6deede83071e42166b4119740e4212b42811
HcwXd <e04dl47878@gmail.com>
2019-10-25 15:57:23 +0800

    Fix eslint problem cause by vs-code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,27 @@
 module.exports = {
   parserOptions: {
-    ecmaVersion: 2017
+    ecmaVersion: 2017,
   },
   extends: ['eslint:recommended', 'prettier'],
   env: {
-    node: true
+    node: true,
   },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+      },
+    ],
+  },
+  plugins: ['prettier'],
   overrides: [
     {
       files: ['**/*.test.js'],
       env: {
-        jest: true
-      }
-    }
-  ]
+        jest: true,
+      },
+    },
+  ],
 };
