9122ce812d616771748132430a7e105d78a83bbf
HcwXd <e04dl47878@gmail.com>
2019-11-26 20:35:50 +0800

    Use mongodb for reminder to send daily reminder

--- reminder/index.js
+++ reminder/index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
-const fs = require('fs');
-const ls = require('ls');
 const axios = require('axios');
 const { redisClient } = require('./services/redis');
 const { REDIS_KEY } = require('./constant');
+const MongoClient = require('mongodb').MongoClient;
 
 const sendUrl = `https://graph.facebook.com/v4.0/me/messages?access_token=${process.env.MESSENGER_ACCESS_TOKEN}`;
 const checkReminderInterval = 60000;
+
+const url = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`;
+const dbName = process.env.MONGO_NAME;
 
 const sendReminder = async ({ id, message }) => {
   await axios.post(sendUrl, {
@@ -38,17 +40,12 @@ const sendTodoReminder = async rep => {
 
 const sendDailyReminder = async rep => {
   const { id } = JSON.parse(rep);
-  const files = ls('../.session/*');
-  for (let file of files) {
-    const rawdata = fs.readFileSync(`../.session/${file.file}`);
-    const jsonData = JSON.parse(rawdata);
-    const { user, _state: state } = jsonData;
-    if (user.id === id) {
-      const message = constuctDailyReminderMessage(state.todos);
-      await sendReminder({ id, message });
-      break;
-    }
-  }
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const Sessions = db.collection('sessions');
+  const { _state: state } = await Sessions.findOne({ id: `messenger:${id}` });
+  const message = constuctDailyReminderMessage(state.todos);
+  await sendReminder({ id, message });
 };
 
 const redisCheckTodoQueue = () => {
